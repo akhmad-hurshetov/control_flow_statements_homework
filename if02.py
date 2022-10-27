a3b9a6306abd30f578f50742bdeb61b03d931a52
Akhmad <akhmadhurshetov@gmail.com>
2022-10-27 12:40:18 +0500

    02

--- if02.py
+++ if02.py
@@ -7,7 +7,7 @@ def main(a):
         a: a increased by 1 if positive, else decreased by 2.
     """
     if a>0:
-        a = a + 1
-    if a<0:
-        a = a - 2
+        a += 1
+    else:
+        a -= 2
     return a

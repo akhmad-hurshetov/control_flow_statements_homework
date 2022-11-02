d1da2a66be8669dd8f9e1491f4df6cf0ab96740e
Akhmad <akhmadhurshetov@gmail.com>
2022-11-02 21:19:50 +0500

    08

--- if08.py
+++ if08.py
@@ -11,4 +11,17 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    two_digit = 9<a<100
+    three_digit = 99<a<1000
+    finish = 0
+
+    if two_digit and a%2!=0:
+        finish = 'two-digit odd number'
+    elif two_digit and a%2==0:
+        finish = 'two-digit even number'
+    elif three_digit and a%2!=0:
+        finish = 'three-digit odd number'
+    elif three_digit and a%2==0:
+        finish = 'three-digit even number'
+    return finish
+print(main(30))
